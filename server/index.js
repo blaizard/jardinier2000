cf282482667c27494fdf786f082390edc76426ee
Blaise <blaise.lengrand@gmail.com>
2019-08-04 11:02:04 +0200

    Implementation of timeseries (wip)

--- server/index.js
+++ server/index.js
@@ -8,12 +8,28 @@ const FileSystem = require("../lib/require/filesystem.js");
 
 // Standard library
 const Path = require("path");
+const TimeSeries = require("../lib/require/persistence/timeseries.js");
 
 (async () => {
+
 	const pathDist = Path.resolve(__dirname, "..", ".dist");
+	const pathData = Path.resolve(__dirname, "..", "data");
 
 	// Create directory if it does not exists
 	await FileSystem.mkdir(pathDist);
+	await FileSystem.mkdir(pathData);
+
+	let timeseries = new TimeSeries(Path.join(pathData, "timeseries"));
+	await timeseries.waitReady();
+	Log.info("Timeseries is ready");
+
+	timeseries.insert(12, "A");
+	timeseries.insert(14, "B");
+	timeseries.insert(11, "C");
+
+	timeseries.forEach((timestamp, data) => {
+		console.log(timestamp, data);
+	}, 0);
 
 	// Set-up the web server
 	let web = new Web(8001, {
